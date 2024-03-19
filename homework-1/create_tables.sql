5dc745d4db1ddfd71efe0675fd52fae4ee1d0449
zetmarley <zzzetmarley@gmail.com>
2024-03-19 11:42:13 +0700

    hm#1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,10 @@
 -- SQL-команды для создания таблиц
+
+CREATE DATABASE north;
+
+CREATE TABLE north_data
+(
+	employees varchar(100) NOT NULL,
+	customers varchar(1000),
+	orders varchar(1000)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,12 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+with psycopg2.connect(
+    host = 'localhost',
+    database = 'north',
+    user = 'postgres',
+    password = '13799731'
+) as conn:
+    with conn.cursor() as cur:
+        cur.executemany('INSERT INTO north_data VALUES (%s, %s, %s)',
+                        [('Dr Mary', 'Darth Vader, Donald Duck', '3'), ('Dr  Blum', 'Timon, Pumba', '10')])
