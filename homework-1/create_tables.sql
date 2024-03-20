44ada8812953ad2ab2c94906aa5906c1263eddd2
Semyon Karaganov <zzzetmarley@gmail.com>
2024-03-20 16:32:55 +0700

    fix hm#1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -2,9 +2,28 @@
 
 CREATE DATABASE north;
 
-CREATE TABLE north_data
+CREATE TABLE employees
 (
-	employees varchar(100) NOT NULL,
-	customers varchar(1000),
-	orders varchar(1000)
+	employee_id int PRIMARY KEY,
+    first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+    title text,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(100),
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(100),
+	employee_id int,
+	order_date date,
+	ship_city varchar(100)
 );
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,6 +1,32 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
 import psycopg2
 
+"""Создаем списки кортежей для БД"""
+customers = []
+employees = []
+orders = []
+
+"""Извлекаем данные CSV в списки"""
+with open('../homework-1/north_data/customers_data.csv', newline='') as csvfile:
+    customers_reader = csv.reader(csvfile)
+    for i in customers_reader:
+        customers.append(tuple(i))
+customers = customers[1::]
+
+with open('../homework-1/north_data/employees_data.csv', newline='') as csvfile:
+    employees_reader = csv.reader(csvfile)
+    for i in employees_reader:
+        employees.append(tuple(i))
+employees = employees[1::]
+
+with open('../homework-1/north_data/orders_data.csv', newline='') as csvfile:
+    orders_reader = csv.reader(csvfile)
+    for i in orders_reader:
+        orders.append(tuple(i))
+orders = orders[1::]
+
+"""Вносим списки с данными в БД"""
 with psycopg2.connect(
     host = 'localhost',
     database = 'north',
@@ -8,5 +34,6 @@ with psycopg2.connect(
     password = '13799731'
 ) as conn:
     with conn.cursor() as cur:
-        cur.executemany('INSERT INTO north_data VALUES (%s, %s, %s)',
-                        [('Dr Mary', 'Darth Vader, Donald Duck', '3'), ('Dr  Blum', 'Timon, Pumba', '10')])
+        cur.executemany('INSERT INTO customers VALUES (%s, %s, %s)', customers)
+        cur.executemany('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', employees)
+        cur.executemany('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', orders)
