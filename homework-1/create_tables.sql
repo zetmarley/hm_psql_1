e97c4d4f0a372bc36d0a4d9f016a46515294ef75
zetmarley <zzzetmarley@gmail.com>
2024-03-21 16:51:30 +0700

    fix#3 hm#1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -14,7 +14,7 @@ CREATE TABLE employees
 
 CREATE TABLE customers
 (
-	customer_id int PRIMARY KEY,
+	customer_id character varying(10) PRIMARY KEY,
 	company_name varchar(100) NOT NULL,
 	contact_name varchar(100) NOT NULL
 );
@@ -22,7 +22,7 @@ CREATE TABLE customers
 CREATE TABLE orders
 (
 	order_id int PRIMARY KEY,
-	customer_id int REFERENCES customers(customer_id),
+	customer_id character varying(10) REFERENCES customers(customer_id),
 	employee_id int REFERENCES employees(employee_id),
 	order_date date,
 	ship_city varchar(100)
