feb922b3ab82fed408dba48bdcac83052b974638
zetmarley <zzzetmarley@gmail.com>
2024-03-23 18:00:24 +0700

    hm#3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,33 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT c.company_name, CONCAT(first_name, ' ', last_name)
+FROM orders as o
+JOIN customers as c USING(customer_id)
+JOIN employees as e USING(employee_id)
+WHERE c.city = e.city and o.ship_via = 2
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT product_name, units_in_stock, contact_name, phone
+FROM products AS p
+JOIN suppliers AS s USING(supplier_id)
+JOIN categories AS c USING(category_id)
+WHERE discontinued = 0
+AND units_in_stock < 25
+AND (c.category_name = 'Dairy Products'
+	or c.category_name = 'Condiments')
+ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+select company_name from customers
+where customer_id = any(select customer_id from customers except
+select distinct customer_id from orders)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+select product_name from products
+where product_id = any(select distinct product_id from order_details
+where quantity = 10)
